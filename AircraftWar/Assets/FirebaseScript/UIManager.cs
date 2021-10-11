93c527bb370e888a6710a5f5b6f7782f027a06ca
HRyanH <huangyuanliryan@gmail.com>
2021-10-11 16:59:39 +0800

    mainmenu-personal page

---
+++ AircraftWar/Assets/FirebaseScript/UIManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager instance;
+
+    //Screen object variables
+    public GameObject loginUI;
+    public GameObject registerUI;
+    public GameObject personalPage;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != null)
+        {
+            Debug.Log("Instance already exists, destroying object!");
+            Destroy(this);
+        }
+    }
+
+    public void ClearScreen()
+    {
+        loginUI.SetActive(false);
+        registerUI.SetActive(false);
+        personalPage.SetActive(false);
+    }
+
+    //Functions to change the login screen UI
+    public void LoginScreen() //Back button
+    {
+        ClearScreen();
+        loginUI.SetActive(true);
+    }
+    public void RegisterScreen() // Regester button
+    {
+        ClearScreen();
+        registerUI.SetActive(true);
+    }
+    public void PersonalPage()
+    {
+        ClearScreen();
+        personalPage.SetActive(true);
+    }
+}
