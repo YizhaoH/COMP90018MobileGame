42758f8daf3c8dde585a2ebc733409385468b7a8
YJC\HP <yateenchiplunkar2010@gmail.com>
2021-11-02 15:52:56 +0530

    Updates

---
+++ AircraftWar/Assets/Volume Control/SoundManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundManager : MonoBehaviour
+{
+	[SerializeField] Slider volumeSlider;
+    void Start()
+    {
+        if ((!PlayerPrefs.HasKey("bgmusic")))
+        {
+            PlayerPrefs.SetFloat("bgmusic", 1);
+            Load();
+        }
+        else
+        {
+            Load();
+        }
+
+    }
+
+    public void changeVolume(){
+	AudioListener.volume = volumeSlider.value;
+	Save();
+}
+    private void Load(){
+    volumeSlider.value = PlayerPrefs.GetFloat("bgmusic");
+}
+
+    private void Save(){
+    PlayerPrefs.SetFloat("bgmusic",volumeSlider.value);
+    }
+}
